059739d09140e6f117087a149a0a1a539c58cae8
ivanadnin <ivandanin01@gmail.com>
2021-05-30 21:45:15 +0300

    built in func

--- Exercise Built-in Functions/geography_database.sql
+++ Exercise Built-in Functions/geography_database.sql
@@ -800,3 +800,17 @@ INSERT INTO `rivers` (`id`, `river_name`, `length`, `drainage_area`, `average_di
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+
+SELECT * FROM `countries`;
+
+-- countries holding 'a' 3 or more times
+ SELECT `country_name`, `iso_code` FROM `countries`
+ WHERE `country_name` LIKE '%A%A%A%'
+ ORDER BY `iso_code`;
+ 
+ -- mix of peaks and river names
+SELECT p.`peak_name`, r.`river_name`, 
+LOWER(CONCAT(`peak_name`, SUBSTRING(`river_name`, 2))) AS 'mix'
+FROM `peaks` AS p, `rivers` AS r 
+WHERE RIGHT(`peak_name`, 1) = LEFT(`river_name`, 1)
+ORDER BY `mix`;
