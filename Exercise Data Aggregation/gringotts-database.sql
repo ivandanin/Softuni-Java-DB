325d0545cf3ca6db21d94096e7d7281e4e091c29
ivanadnin <ivandanin01@gmail.com>
2021-06-03 15:33:59 +0300

    dae

--- Exercise Data Aggregation/gringotts-database.sql
+++ Exercise Data Aggregation/gringotts-database.sql
@@ -262,3 +262,11 @@ FROM `wizzard_deposits`
 WHERE `deposit_group` = 'Troll Chest'
 GROUP BY `first_letter`
 ORDER BY `first_letter`; 
+
+-- average interest
+SELECT `deposit_group`, `is_deposit_expired`,
+AVG(`deposit_interest`) AS `average_interest`
+FROM `wizzard_deposits`
+WHERE `deposit_start_date` > '1985-01-01'
+GROUP BY `deposit_group`, `is_deposit_expired`
+ORDER BY `deposit_group` DESC, `is_deposit_expired` ASC;
