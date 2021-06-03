9b1fcff5291a84350b517fe797a39f1178daa9db
ivanadnin <ivandanin01@gmail.com>
2021-06-03 14:55:18 +0300

    data agregation ex

--- Exercise Data Aggregation/gringotts-database.sql
+++ Exercise Data Aggregation/gringotts-database.sql
@@ -196,3 +196,69 @@ FROM `wizzard_deposits`;
 SELECT MAX(`magic_wand_size`)
 AS `longest_magic_wand` 
 FROM `wizzard_deposits`; 
+
+-- longest magic wand per deposit groups
+SELECT DISTINCT `deposit_group`, MAX(`magic_wand_size`)
+AS `longest_magic_wand` 
+FROM `wizzard_deposits`
+GROUP BY `deposit_group`
+ORDER BY `longest_magic_wand`, `deposit_group`; 
+
+-- smallest deposit group per magic wand size
+SELECT `deposit_group` FROM `wizzard_deposits`
+GROUP BY `deposit_group`
+ORDER BY MIN(`magic_wand_size`)
+LIMIT 1;
+ 
+-- deposits sum
+SELECT `deposit_group`,
+SUM(`deposit_amount`) AS `total_sum` 
+FROM `wizzard_deposits`
+GROUP BY `deposit_group`
+ORDER BY `total_sum`;
+
+-- deposits sum for ollivander family
+SELECT `deposit_group`, 
+SUM(`deposit_amount`) AS `total_sum` 
+FROM `wizzard_deposits`
+WHERE `magic_wand_creator` = 'Ollivander family'
+GROUP BY `deposit_group`
+ORDER BY `deposit_group`;
+
+-- deposits filter
+SELECT `deposit_group`, 
+SUM(`deposit_amount`) AS `total_sum` 
+FROM `wizzard_deposits`
+WHERE `magic_wand_creator` = 'Ollivander family'
+GROUP BY `deposit_group`
+HAVING `total_sum` < 150000
+ORDER BY `total_sum` DESC;
+
+-- deposit charge
+SELECT `deposit_group`, `magic_wand_creator`, 
+MIN(`deposit_charge`) AS `min_deposit_charge`
+FROM `wizzard_deposits`
+GROUP BY `deposit_group`, `magic_wand_creator`
+ORDER BY `magic_wand_creator` ASC, `deposit_group` ASC;
+
+-- age groups
+SELECT (CASE 
+WHEN `age` BETWEEN 0 AND 10 THEN '[0-10]'
+WHEN `age` BETWEEN 11 AND 20 THEN '[11-20]'
+WHEN `age` BETWEEN 21 AND 30 THEN '[21-30]'
+WHEN `age` BETWEEN 31 AND 40 THEN '[31-40]'
+WHEN `age` BETWEEN 41 AND 50 THEN '[41-50]'
+WHEN `age` BETWEEN 51 AND 60 THEN '[51-60]'
+ELSE '[61+]'
+END) AS `age_group`,
+COUNT(*) AS `wizard_count`
+FROM `wizzard_deposits`
+GROUP BY `age_group`
+ORDER BY `age_group`;
+ 
+-- first letter
+SELECT LEFT(`first_name`, 1) AS 'first_letter'
+FROM `wizzard_deposits`
+WHERE `deposit_group` = 'Troll Chest'
+GROUP BY `first_letter`
+ORDER BY `first_letter`; 
