45b165954079856aa47bb2332d02aba64b698457
ivanadnin <ivandanin01@gmail.com>
2021-06-05 16:00:16 +0300

    table relations

--- camp_db.sql
+++ camp_db.sql
@@ -88,3 +88,30 @@ CREATE TABLE `peaks`
 CONSTRAINT fk_mountains_peaks FOREIGN KEY 
 (`mountain_id`) REFERENCES `mountains`(`id`));
 
+-- trip organization
+SELECT v.`driver_id`, v.`vehicle_type`,
+CONCAT(c.`first_name`, ' ', c.`last_name`) AS `driver_name`
+FROM `vehicles`AS v JOIN `campers` AS c
+ON v.`driver_id` = c.`id`;
+
+-- softuni hiking
+SELECT `starting_point` AS `route_starting_point`,
+`end_point` AS `route_end_point`,
+`leader_id`,
+CONCAT(c.`first_name`, ' ', c.`last_name`) AS `leader_name`
+FROM `routes` AS r 
+JOIN `campers` AS c
+ON 	r.`leader_id` = c.`id`;
+
+-- delete mountains
+CREATE TABLE `mountains`(
+`id` INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(20) NOT NULL);
+CREATE TABLE `peaks`(
+`id` INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(20) NOT NULL,
+`mountain_id` INT,
+CONSTRAINT `fk_mountain_id`
+FOREIGN KEY(`mountain_id`)
+REFERENCES `mountains`(`id`)
+ON DELETE CASCADE);
