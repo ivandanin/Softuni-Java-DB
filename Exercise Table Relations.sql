06c7b35ca2abbcd35ecee9e13d9754531551f2a4
ivanadnin <ivandanin01@gmail.com>
2021-06-07 21:50:25 +0300

    table rel

--- Exercise Table Relations.sql
+++ Exercise Table Relations.sql
@@ -2,6 +2,7 @@ CREATE DATABASE `table_relations`;
 
 USE `table_relations`;
 
+-- one to onw relationship 
 CREATE TABLE `passports`
 (`passport_id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 `passport_number` VARCHAR(8))
@@ -17,7 +18,7 @@ CREATE TABLE `people`
 `first_name` VARCHAR(50),
 `salary` DECIMAL(10, 2),
 `passport_id` INT NOT NULL UNIQUE,
-CONSTRAINT fk_people_passportS
+CONSTRAINT fk_people_passports
 FOREIGN KEY (`passport_id`)
 REFERENCES `passports`(`passport_id`));
 
@@ -26,5 +27,32 @@ VALUES ('Roberto', 43300, 102),
 ('Tom', 56100, 103),
 ('Yana', 60200, 101);
 
+-- one to many relationship
+CREATE TABLE `manufacturers`
+(`manufacturer_id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(30),
+`established_on` DATE);
+
+INSERT INTO `manufacturers`
+VALUES (1, 'BMW', '1916-03-01'),
+(2, 'Tesla', '2003-01-01'),
+(3, 'Lada', '1966-05-01');
+
+CREATE TABLE `models`
+(`model_id` INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(30),
+`manufacturer_id` INT,
+CONSTRAINT fk_manufacturers_models
+FOREIGN KEY (`manufacturer_id`)
+REFERENCES `manufacturers`(`manufacturer_id`))
+AUTO_INCREMENT = 101;
+
+INSERT INTO `models`
+VALUES (101, 'X1', 1),
+(102, 'i6', 1),
+(103, 'Model S', 2),
+(104, 'Model X', 2),
+(105, 'Model 3', 2), 
+(106, 'Nova', 3);
 
 SELECT * FROM `pASSPORTS`;
