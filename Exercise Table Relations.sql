760c28d470691217c1116f68941f052aa3228755
ivanadnin <ivandanin01@gmail.com>
2021-06-08 13:07:25 +0300

    table realtions

--- Exercise Table Relations.sql
+++ Exercise Table Relations.sql
@@ -160,4 +160,41 @@ CONSTRAINT fk_items_order_items
 FOREIGN KEY (`item_id`)
 REFERENCES `items`(`item_id`));
 
-SELECT * FROM `students_exams`;
+-- university database
+CREATE TABLE `majors`
+(`major_id` INT(11) PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(50));
+ 
+ CREATE TABLE `students`
+(`student_id` INT(11) PRIMARY KEY AUTO_INCREMENT,
+`student_number` VARCHAR(12),
+`student_name` VARCHAR(50),
+ `major_id` INT(11),
+ CONSTRAINT fk_majors_students
+ FOREIGN KEY (`major_id`)
+ REFERENCES `majors`(`major_id`));
+ 
+ CREATE TABLE `payments`
+ (`payment_id` INT(11) PRIMARY KEY AUTO_INCREMENT,
+ `payment_date` DATE,
+ `payment_amount` DECIMAL(8, 2),
+ `student_id` INT(11),
+ CONSTRAINT fk_students_payments
+ FOREIGN KEY (`student_id`)
+ REFERENCES `students`(`student_id`));
+
+CREATE TABLE `subjects`
+(`subject_id`  INT(11) PRIMARY KEY AUTO_INCREMENT,
+`subject_name` VARCHAR(50));
+
+CREATE TABLE `agenda`
+(`student_id` INT(11),
+`subject_id` INT(11),
+CONSTRAINT pk_agenda
+PRIMARY KEY (`student_id`, `subject_id`),
+CONSTRAINT fk_students_agenda
+FOREIGN KEY (`student_id`)
+REFERENCES `students`(`student_id`),
+CONSTRAINT fk_subjects_agenda
+FOREIGN KEY (`subject_id`)
+REFERENCES `subjects`(`subject_id`));
