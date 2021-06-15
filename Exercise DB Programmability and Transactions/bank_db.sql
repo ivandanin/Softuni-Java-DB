f2f3cee21c8c4834abda19e20da120c13dc4eb8f
ivanadnin <ivandanin01@gmail.com>
2021-06-15 20:17:20 +0300

    database programmability exercise

--- Exercise DB Programmability and Transactions/bank_db.sql
+++ Exercise DB Programmability and Transactions/bank_db.sql
@@ -85,17 +85,17 @@ DELIMITER ;
 DELIMITER //
 CREATE PROCEDURE `usp_get_holders_with_balance_higher_than`(`comparative_value` DECIMAL(19, 4))
 BEGIN
-	SELECT `first_name`, `last_name` 
+	SELECT ah.`first_name`, ah.`last_name` 
 	FROM `account_holders` AS ah
 	JOIN `accounts` AS a
 	ON a.`account_holder_id` = ah.`id`
-	WHERE `balance` > `comparative_value`
-    GROUP BY ah.`id`
-	ORDER BY ah.`id`;
+    GROUP BY a.`account_holder_id`
+    HAVING SUM(a.`balance`) > `comparative_value`
+	ORDER BY a.`account_holder_id`;
 END //
 DELIMITER ;
 
--- future value functions
+-- future value functions 
 DELIMITER //
 CREATE FUNCTION `ufn_calculate_future_value`(`sum` DECIMAL(19, 4), `interest` DOUBLE, `years` INT)
 RETURNS DECIMAL(19, 4)  
@@ -104,3 +104,127 @@ BEGIN
 	RETURN `sum` * POW(1 + `interest`, `years`);
 END //
 DELIMITER ;
+
+-- calculating interest (future value functions needed in judge)
+DELIMITER //
+CREATE PROCEDURE `usp_calculate_future_value_for_account`(`acc_id` INT, `interest` DECIMAL(19,4)) 
+BEGIN
+	SELECT a.`id` AS `account_id`,
+	ah.`first_name`,
+	ah.`last_name`,
+	a.`balance` AS `current_balance`,
+	`ufn_calculate_future_value`(a.`balance`, `interest`, 5) AS `balance_in_5_years` 
+	FROM `accounts` AS a
+	JOIN `account_holders` AS ah
+	ON a.`account_holder_id` = ah.`id`
+	WHERE a.`id` = `acc_id`;
+END //
+DELIMITER ;
+call `usp_calculate_future_value_for_account`(1, 0.1);
+
+-- deposit money
+DELIMITER //
+CREATE PROCEDURE `usp_deposit_money`(`account_id` INT, `money_amount` DECIMAL(19, 4)) 
+BEGIN
+	IF(SELECT COUNT(*) FROM `accounts`
+		WHERE `id` = `account_id`) = 0
+		OR (`money_amount` <= 0)
+		THEN ROLLBACK;
+    ELSE 
+		UPDATE `accounts`
+        SET `balance` = `balance` + `money_amount`
+        WHERE `id` = `account_id`;
+	END IF;
+END //
+DELIMITER ;
+
+-- withdraw money
+DELIMITER //
+CREATE PROCEDURE `usp_withdraw_money`(`account_id` INT, `money_amount` DECIMAL(19, 4)) 
+BEGIN
+	START TRANSACTION;
+	IF(SELECT COUNT(*) FROM `accounts`
+		WHERE `id` = `account_id`) = 0
+		OR (`money_amount` <= 0)
+        OR ((SELECT `balance` FROM `accounts`
+        WHERE `id` = `account_id`) <= `money_amount`)
+		THEN ROLLBACK;
+    ELSE 
+		UPDATE `accounts`
+        SET `balance` = `balance` - `money_amount`
+        WHERE `id` = `account_id`;
+	END IF;
+END  //
+DELIMITER ;
+
+-- transfer money
+DELIMITER //
+CREATE PROCEDURE `usp_transfer_money`(`from_account_id` INT, `to_account_id` INT, `amount` DECIMAL(19, 4))
+BEGIN
+	START TRANSACTION;
+		IF (SELECT `id` FROM `accounts` 
+		WHERE `id` = `from_account_id`) IS NULL
+		OR (SELECT `id` FROM `accounts` 
+		WHERE `id` = `to_account_id`) IS NULL
+		OR `from_account_id` = `to_account_id`
+		OR `amount` < 0
+		OR ((SELECT `balance` FROM `accounts`
+		WHERE `id` = `from_account_id`) - `amount` < 0)
+		THEN ROLLBACK;
+    ELSE 
+		UPDATE `accounts`
+        SET `balance` = `balance` - `amount`
+		WHERE `id` = `from_account_id`;
+        UPDATE `accounts`
+        SET `balance` = `balance` + `amount`
+        WHERE `id` = `to_account_id`;
+    END IF;
+END //
+DELIMITER ;
+
+-- log accounts trigger
+CREATE TABLE `logs` 
+(`log_id` INT PRIMARY KEY AUTO_INCREMENT, 
+`account_id` INT,
+`old_sum` DECIMAL(19, 4),
+`new_sum` DECIMAL(19, 4));
+
+DELIMITER //
+CREATE TRIGGER `tr_update_accounts`
+AFTER UPDATE
+ON `accounts`
+FOR EACH ROW
+BEGIN
+	INSERT INTO `logs` (`account_id`, `old_sum`, `new_sum`)
+    VALUES  (OLD.`id`, OLD.`balance`, NEW.`balance`);
+END //
+DELIMITER ;
+
+-- emails triger
+CREATE TABLE `logs` 
+(`log_id` INT UNSIGNED PRIMARY KEY AUTO_INCREMENT, 
+`account_id` INT,
+`old_sum` DECIMAL(19, 4),
+`new_sum` DECIMAL(19, 4));
+
+CREATE TABLE `notification_emails`
+(`id` INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+`recipient` INT, 
+`subject` VARCHAR(255),
+`body` TEXT);
+
+DELIMITER //
+CREATE TRIGGER `tr_notification`
+AFTER UPDATE
+ON `accounts`
+FOR EACH ROW
+BEGIN 
+	INSERT INTO `logs` (`account_id`, `old_sum`, `new_sum`)
+    VALUES  (OLD.`id`, OLD.`balance`, NEW.`balance`);
+    
+	INSERT INTO `notification_emails` (`recipient`, `subject`, `body`)
+    VALUES (OLD.`id`,
+    CONCAT('Balance change for account: ', OLD.`id`),
+    CONCAT('On ', NOW(), ' your balance was changed from ', OLD.`balance`, ' to ', NEW.`balance`, '.'));
+END //
+DELIMITER ; 
---
+++ MySQL/Data Definition and Data Types.sql
@@ -0,0 +1,34 @@
+CREATE schema `gamebar`;
+
+CREATE TABLE `employees` (`id` INT PRIMARY KEY AUTO_INCREMENT,
+ `first_name` VARCHAR(50) NOT NULL,
+ `last_name` VARCHAR(50) NOT NULL);
+
+CREATE TABLE `categories` (`id` INT PRIMARY KEY AUTO_INCREMENT,
+ `name` VARCHAR(50) NOT NULL);
+
+CREATE TABLE `products` (`id` INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(50) NOT NULL,
+`category_id` INT NOT NULL);
+/* creating tables */
+
+INSERT INTO `employees` (`first_name`, `last_name`) VALUES ('Test1', 'Tets1');
+INSERT INTO `employees` (`first_name`, `last_name`) VALUES ('Test2', 'Test2');
+INSERT INTO `employees` (`first_name`, `last_name`) VALUES ('Test3', 'Tets3');
+/* inserting data */
+
+ ALTER TABLE `employees`
+ ADD COLUMN `middle_name` VARCHAR(50) NOT NULL;
+ /* altering tables */
+ 
+ ALTER TABLE `products`
+ ADD CONSTRAINT fk_products_categories
+ FOREIGN KEY (`category_id`)
+ REFERENCES `categories`(`id`);
+ /* adding constraints */
+ 
+ ALTER TABLE `employees`
+MODIFY COLUMN `middle_name` 
+VARCHAR(100);
+/* modifying columns */
+ 
