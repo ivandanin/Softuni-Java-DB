7ab02d480ff54885f643898e9d18c5487e7bdec9
ivanadnin <ivandanin01@gmail.com>
2021-06-17 21:49:44 +0300

    exam prep2

--- colonial_journey_management_system_db_examprep2018oct21.sql
+++ colonial_journey_management_system_db_examprep2018oct21.sql
@@ -472,3 +472,56 @@ ON p.`id` = s.`planet_id`
 ORDER BY DATEDIFF(`journey_end`, `journey_start`)
 LIMIT 1;
 
+-- extract the less popular job
+SELECT tc.`job_during_journey`
+FROM `travel_cards` AS tc
+WHERE tc.`journey_id` = (
+SELECT j.`id`
+FROM `journeys` AS j
+ORDER BY DATEDIFF(`journey_end`, `journey_start`) DESC
+LIMIT 1)
+GROUP BY tc.`job_during_journey`
+ORDER BY COUNT(tc.`job_during_journey`)
+LIMIT 1;
+
+-- get colonists count
+DELIMITER //
+CREATE FUNCTION `udf_count_colonists_by_destination_planet`(`planet_name` VARCHAR(30)) 
+RETURNS INT
+DETERMINISTIC
+BEGIN
+	DECLARE c_count INT;
+    SET c_count := (
+	SELECT COUNT(c.`id`)
+    FROM `colonists` AS c
+    JOIN `travel_cards` AS tc
+    ON tc.`colonist_id` = c.`id`
+    JOIN `journeys` AS j
+    ON j.`id` = tc.`journey_id`
+    JOIN `spaceports` AS s
+    ON s.`id` = j.`destination_spaceport_id`
+    JOIN `planets` AS p
+	ON p.`id` = s.`planet_id`
+    WHERE p.`name` = `planet_name`);
+    RETURN c_count;
+END //
+DELIMITER ;
+
+-- modify spaceships
+DELIMITER //
+CREATE PROCEDURE `udp_modify_spaceship_light_speed_rate`(
+`spaceship_name` VARCHAR(50), `light_speed_rate_increse` INT(11)) 
+BEGIN
+	IF (SELECT COUNT(ss.`name`)
+	FROM `spaceships` AS ss
+	WHERE ss.`name` = `spaceship_name` > 0)
+	THEN UPDATE `spaceships`
+	SET ss.`light_speed_rate` = ss.`light_speed_rate` + `light_speed_rate_increse`
+	WHERE ss.`name` = `spaceship_name`;
+    ELSE 
+    SIGNAL SQLSTATE '45000'
+    SET MESSAGE_TEXT = 'Spaceship you are trying to modify does not exists.';
+    ROLLBACK;
+    END IF;
+END //
+DELIMITER ;
